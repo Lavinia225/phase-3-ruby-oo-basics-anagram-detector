ff8d000a0be0e55495b75bf2a1b591b56968779a
Luna Seifert <thepuddingdeity@gmail.com>
2023-03-14 14:34:56 -0500

    commit tree 4dae0d343605f46caa55abdfcee1475dae1317ab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match word_array
+        test_word = word.chars.sort
+        iterator = 0
+        return_array = []
+
+        modified_array = word_array.map do |word|
+            split_word = word.chars
+            sorted_letters = split_word.sort
+        end
+
+        while iterator < word_array.length
+            if test_word == modified_array[iterator]
+                return_array.push(word_array[iterator])
+            end
+            iterator += 1
+        end
+        return_array
+    end
+
+end
